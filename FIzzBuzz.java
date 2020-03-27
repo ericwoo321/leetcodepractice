e421928d60121a9f2ba7ea7f7204192936c51f7f
ericwoo321 <38222860+ericwoo321@users.noreply.github.com>
2020-03-27 14:07:30 -0400

    Add files via upload

---
+++ FIzzBuzz.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList <String> result = new ArrayList<>();  
+        if(n==1){
+             result.add(1+"");
+            return result;
+        }
+      
+        for(int i =1; i<=n; i++){
+            if(i%5 ==0 && i%3 ==0){
+               result.add("FizzBuzz");
+            }
+            else if(i%3 ==0){
+             result.add("Fizz"); 
+            }
+             else  if(i%5 ==0){
+                result.add("Buzz"); 
+            }
+            
+            else 
+                result.add(i+""); 
+          
+        }
+        return result;
+    }
+}
